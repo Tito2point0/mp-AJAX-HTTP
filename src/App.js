1ea66552e86c6ea89b4201ceaa01c080ca2900d2
sanjiro <defconx00@gmail.com>
2023-10-09 17:35:55 -0400

    finished mvp

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import React, { useEffect, useState } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import MovieList from './components/MovieList';
 import Movie from './components/Movie';
+import EditMovieForm from "./components/EditMovieForm";
 
 import MovieHeader from './components/MovieHeader';
 
 import FavoriteMovieList from './components/FavoriteMovieList';
-
+import AddMovieForm from "./components/AddMovieForm";
 import axios from 'axios';
 
 const App = (props) => {
@@ -25,10 +26,15 @@ const App = (props) => {
   }, []);
 
   const deleteMovie = (id) => {
+    setMovies(movies.filter(item => {
+      item.id !== id
+    }))
   }
 
   const addToFavorites = (movie) => {
-
+    setFavoriteMovies(movies.filter(item => {
+  item.id === movie.id
+}))
   }
 
   return (
@@ -41,13 +47,15 @@ const App = (props) => {
         <MovieHeader />
         <div className="row ">
           <FavoriteMovieList favoriteMovies={favoriteMovies} />
+             <Routes>
+            <Route path="/movies/add" element={<AddMovieForm setMovies={setMovies}/>}/> 
+            <Route path="movies/edit/:id" element={<EditMovieForm setMovies={setMovies } />} />
 
-          <Routes>
-            <Route path="movies/edit/:id" />
-
-            <Route path="movies/:id" />
+            <Route path="movies/:id" element={<Movie deleteMovie={deleteMovie} addToFavorites={addToFavorites} />} />
 
             <Route path="movies" element={<MovieList movies={movies} />} />
+
+            
 
             <Route path="/" element={<Navigate to="/movies" />} />
           </Routes>
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams, useNavigate } from 'react-router-dom';
 
+
 import axios from 'axios';
 
 const Movie = (props) => {
-  const { addToFavorites } = props;
+  const { addToFavorites, deleteMovie } = props;
 
   const [movie, setMovie] = useState('');
 
@@ -20,6 +21,26 @@ const Movie = (props) => {
         console.log(err.response);
       })
   }, [id]);
+
+  const handleDelete = () => {
+    axios.delete(`http://localhost:9000/api/movies/${id}`)
+      .then(() => {
+        deleteMovie(id);
+        navigate('/movies')
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+  const handleFavorites = () => {
+    axios.get(`http://localhost:9000/api/movies/${id}`)
+      .then(res => {
+        addToFavorites(res.data)
+      })
+      .catch(err => {
+      console.error(err)
+    })
+  }
 
   return (<div className="modal-page col">
     <div className="modal-dialog">
@@ -50,9 +71,9 @@ const Movie = (props) => {
             </section>
 
             <section>
-              <span className="m-2 btn btn-dark">Favorite</span>
+              <span className="m-2 btn btn-dark" onClick={handleFavorites}>Favorite</span>
               <Link to={`/movies/edit/${movie.id}`} className="m-2 btn btn-success">Edit</Link>
-              <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete" /></span>
+              <span className="delete" onClick={handleDelete}><input type="button" className="m-2 btn btn-danger" value="Delete" /></span>
             </section>
           </div>
         </div>
